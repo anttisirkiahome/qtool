c7105a5898ef6593d7fe26265710935b36afe54a
antti <sirkia.antti@gmail.com>
2014-05-28 18:51:36 +0300

    proof of concept

--- app/scripts/services/poller.js
+++ app/scripts/services/poller.js
@@ -5,7 +5,7 @@ var qtoolServices = angular.module('qtoolServices', ['ngResource']);
 qtoolServices.factory('Poller', function($http, $timeout) {
   var data = { response: {}, calls: 0 };
   var poller = function() {
-    $http.get('data.json').then(function(r) {
+    $http.get('//localhost/qtool-backend/api.php').then(function(r) {
       data.response = r.data;
       data.calls++;
       $timeout(poller, 1000);
