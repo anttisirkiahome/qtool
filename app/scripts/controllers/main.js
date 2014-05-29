5e700e121e2a2344be87a61498d973ed6e53d946
antti <sirkia.antti@gmail.com>
2014-05-29 15:38:53 +0300

    new poll features

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,8 @@ qtoolControllers.controller('AdminCtrl', function ($scope, $window) {
 	//init app status
 	$scope.creatingNewPoll = false;
 	$scope.currentTemplate = 'defaultTemplate';
+	$scope.question = '';
+	$scope.answers = [''];
 
 	$scope.loginSubmit = function() {
 		//FIXME temp solution
@@ -20,5 +22,34 @@ qtoolControllers.controller('AdminCtrl', function ($scope, $window) {
 	$scope.switchTemplate = function(template) {
 		$scope.currentTemplate = template;
 	}
+
+	$scope.addAnswer = function() {
+		if($scope.answers.length < 6) {
+			$scope.answers.push('');	
+		}
+	}
+
+	$scope.removeAnswer = function(index) {
+		if($scope.answers.length > 1) {
+			$scope.answers.splice($scope.answers.indexOf($scope.answers[index]), 1);
+		}
+	}
+
+	$scope.moveAnswerUp = function(index) {
+		var oldValue = $scope.answers[index];
+		var newValue = $scope.answers[(index -1)];
+
+		$scope.answers[index] = newValue;
+		$scope.answers[(index -1)] = oldValue;
+	}
+
+	$scope.moveAnswerDown = function(index) {
+		var oldValue = $scope.answers[index];
+		var newValue = $scope.answers[(index + 1)];
+
+		$scope.answers[index] = newValue;
+		$scope.answers[(index + 1)] = oldValue;
+	}
+
 
 });
