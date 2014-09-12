9a1be522d4f61dcae6c39dcdede27191bf864fc7
anttisirkiahome <antti.sirkia@futurice.com>
2014-09-12 15:22:59 +0300

    x

--- Gruntfile.js
+++ Gruntfile.js
@@ -307,7 +307,8 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'fonts/*'
+            'fonts/*',
+            'bower_components/fontawesome/fonts/*.*'
           ]
         }, {
                 expand: true,
